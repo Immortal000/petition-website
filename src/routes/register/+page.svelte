3c0dbfa67c2be71ef29918808153daac89be7623
siddarth kotthur <kotthursiddarth@gmail.com>
2023-07-28 22:31:08 -0500

    refactor

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,15 +1,8 @@
 <script>
 	import { userAuth } from '../../store';
-	import Login from '../../components/login.svelte';
-	import Register from '../../components/Register.svelte';
+	import UserLogin from '../../components/user_login.svelte';
 
 	let login = true;
 </script>
 
-{#if $userAuth.user_id != null}
-	<h1>Already logged in!</h1>
-{:else if $userAuth.login_screen == true}
-	<Login />
-{:else}
-	<Register />
-{/if}
+<UserLogin />
